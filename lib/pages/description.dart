e657945d014c1250f1bcd05dfd6293eae692c0b2
RonnyK36 <ronnykelvyne3@gmail.com>
2021-07-19 13:32:31 +0300

    Homepage UI

--- lib/pages/description.dart
+++ lib/pages/description.dart
@@ -14,6 +14,5 @@ class Description extends StatelessWidget {
         ],
       ),
     );
-    ;
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
+import 'package:bid_palour/config/config.dart';
 import 'package:bid_palour/widgets/app_bar.dart';
+import 'package:bid_palour/widgets/button.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,15 +12,41 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: header(context, titleText: 'Bid Palour'),
       body: ListView(
         children: [
           Column(
-            children: [Text('Home page')],
+            children: [
+              Text('Home page'),
+            ],
+          ),
+          Card(
+            color: Colors.white,
+            elevation: 5,
+            child: Container(
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('images/hundred.jpeg'))),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 50,
+                child: reusableButton(onPressed: () {}, buttonName: "Bid now"),
+              )
+            ],
           ),
         ],
       ),
     );
-    ;
   }
 }
