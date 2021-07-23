832018fcc1bcc8505b133d80799d0beebf6d2ca6
RonnyK36 <ronnykelvyne3@gmail.com>
2021-07-23 20:11:17 +0300

    Added new screen - Active Bids

---
+++ lib/pages/active_bids.dart
@@ -0,0 +1,60 @@
+import 'package:bid_palour/config/config.dart';
+import 'package:flutter/material.dart';
+
+class ActiveBids extends StatefulWidget {
+  const ActiveBids({Key? key}) : super(key: key);
+
+  @override
+  _ActiveBidsState createState() => _ActiveBidsState();
+}
+
+class _ActiveBidsState extends State<ActiveBids> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Text('Active bids'),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 200,
+              width: double.infinity,
+              // color: Colors.green,
+              child: Card(
+                elevation: 5,
+                child: Column(
+                  children: [
+                    Text(
+                      'Your current Balance:',
+                      style: kAccountText,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Ksh. ',
+                          style: TextStyle(
+                            fontSize: 17,
+                          ),
+                        ),
+                        Text(
+                          '100',
+                          style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
