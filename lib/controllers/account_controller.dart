51949d79ed6eb6023ed35fde3cd649a6d6cfa84d
Akron254 <aaronrono42@gmail.com>
2021-07-26 14:32:28 +0300

    Account and user controller

--- lib/controllers/account_controller.dart
+++ lib/controllers/account_controller.dart
@@ -1,6 +1,8 @@
+import 'package:bid_palour/models/user_model.dart';
 import 'package:get/get.dart';
 
 class AccountController extends GetxController {
+  Rx<UserModel> _userModel = UserModel().obs;
   final accountNumber = ''.obs;
   final ownerId = ''.obs;
   final balance = 0.0.obs;
@@ -8,10 +10,16 @@ class AccountController extends GetxController {
   final winnings = 0.0.obs;
   final pendingPayments = 0.obs;
   final completedPayments = 0.obs;
+  UserModel get user => _userModel.value;
+  set user(UserModel value) => this._userModel.value = value;
 
   @override
   void onInit() {
     // TODO: implement fetch account information from database
     super.onInit();
+  }
+
+  void clear() {
+    _userModel.value = UserModel();
   }
 }
