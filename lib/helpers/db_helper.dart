e33c3316ef31e38d78a1d6429f4a0555552ba33c
Akron254 <aaronrono42@gmail.com>
2021-07-27 20:22:34 +0300

    Added bid and sessions db handlers

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -1,3 +1,4 @@
+import 'package:bid_palour/models/bid_model.dart';
 import 'package:bid_palour/models/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -46,6 +47,56 @@ class Database {
       });
     } catch (e) {
       print(e);
+    }
+  }
+
+  Future<bool> createBid(BidModel bid) async {
+    try {
+      await _firestore.collection("bids").doc(bid.id).set({
+        "amount": bid.amount,
+      });
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  Future<BidModel> getBids(String uid) async {
+    try {
+      DocumentSnapshot _doc =
+          await _firestore.collection("bids").doc(uid).get();
+
+      return BidModel.fromDocumentSnapshot(documentSnapshot: _doc);
+    } catch (e) {
+      print(e);
+      rethrow;
+    }
+  }
+
+  Future<bool> addCreateSession(BidModel bid, int value) async {
+    try {
+      await _firestore.collection("sessions").doc(bid.id).set({
+        "amount": bid.amount,
+        "value": value,
+        "bidders": bid.id,
+      });
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  Future<void> getSession(String uid) async {
+    try {
+      DocumentSnapshot _doc =
+          await _firestore.collection("sessions").doc(uid).get();
+
+      //TODO: Implement session handling
+    } catch (e) {
+      print(e);
+      rethrow;
     }
   }
 }
