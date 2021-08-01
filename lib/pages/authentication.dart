2e4c85945fac529bba5f003a25ffcf7747e67033
RonnyK36 <ronnykelvyne3@gmail.com>
2021-08-01 11:44:18 +0300

    "Authentication UI created"

--- lib/pages/authentication.dart
+++ lib/pages/authentication.dart
@@ -2,6 +2,7 @@ import 'package:bid_palour/pages/account.dart';
 import 'package:bid_palour/pages/active_bids.dart';
 import 'package:bid_palour/pages/description.dart';
 import 'package:bid_palour/pages/home.dart';
+import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -44,7 +45,74 @@ class _NavigationState extends State<Navigation> {
   Scaffold buildAuthPage() {
     return Scaffold(
       body: Container(
-        color: Colors.green,
+        // color: Colors.green,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Spacer(),
+              Text(
+                // 'Welcome to bid palor',
+                'Create an account',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    height: 70,
+                    child: CountryCodePicker(
+                      initialSelection: 'Kenya',
+                      textStyle: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
+                      showFlag: true,
+                      enabled: true,
+                      // showDropDownButton: true,
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    child: Form(
+                      child: TextFormField(
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                          labelText: 'Phone Number',
+                          hintText: 'eg 727447786',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              // SizedBox(height: 100),
+              Spacer(),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    'Verify your phone number to create your account and start bidding.',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
+                      letterSpacing: 1.5,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+            ],
+          ),
+        ),
       ),
     );
   }
