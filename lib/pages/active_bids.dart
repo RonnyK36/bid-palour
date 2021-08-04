159e8fbed9955bc9d320fb395bf507b5a905c78c
RonnyK36 <ronnykelvyne3@gmail.com>
2021-08-04 23:53:23 +0300

    Active bids UI

--- lib/pages/active_bids.dart
+++ lib/pages/active_bids.dart
@@ -14,52 +14,73 @@ class _ActiveBidsState extends State<ActiveBids> {
     return Scaffold(
       body: ListView(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              // color: Colors.green,
-              child: Card(
-                elevation: 5,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      'Your current Balance:',
-                      style: kAccountText,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Ksh. ',
-                          style: TextStyle(
-                            fontSize: 17,
-                          ),
-                        ),
-                        Text(
-                          '100',
-                          style: TextStyle(
-                            fontSize: 40,
-                            color: Colors.green,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
           Container(
             height: 50,
             width: double.infinity,
-            color: Colors.green,
+            decoration: BoxDecoration(
+              color: Colors.green,
+            ),
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('Bidding on KES 500'),
+                  Row(
+                    children: [
+                      Text('2'),
+                      Text('Bids'),
+                    ],
+                  ),
+                  Text(
+                    '17-08-2021 9.36 am',
+                  ),
+                ],
+              ),
+            ]),
           ),
         ],
       ),
     );
   }
 }
+
+
+//  Padding(
+//             padding: const EdgeInsets.all(8.0),
+//             child: Container(
+//               height: 100,
+//               width: double.infinity,
+//               // color: Colors.green,
+//               child: Card(
+//                 elevation: 5,
+//                 child: Column(
+//                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                   children: [
+//                     Text(
+//                       'Your current Balance:',
+//                       style: kAccountText,
+//                     ),
+//                     Row(
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       children: [
+//                         Text(
+//                           'Ksh. ',
+//                           style: TextStyle(
+//                             fontSize: 17,
+//                           ),
+//                         ),
+//                         Text(
+//                           '100',
+//                           style: TextStyle(
+//                             fontSize: 40,
+//                             color: Colors.green,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ),
